413182f7ad88b8b9cccf22ae83106c62bc6eee33
Fazil-shaik <ammia4770@gmail.com>
2025-03-17 11:28:23 +0530

    changes of port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
